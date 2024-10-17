ece4b2dbfef243f2589d1247236a472adfb549ec
Lizeth-V <Lizeth.valdovinosrodriguez01@student.csulb.edu>
2024-10-17 14:07:36 -0700

    Added button for "24 cupcakes"

--- app/src/main/java/com/example/cupcake/data/DataSource.kt
+++ app/src/main/java/com/example/cupcake/data/DataSource.kt
@@ -29,6 +29,8 @@ object DataSource {
     val quantityOptions = listOf(
         Pair(R.string.one_cupcake, 1),
         Pair(R.string.six_cupcakes, 6),
-        Pair(R.string.twelve_cupcakes, 12)
+        Pair(R.string.twelve_cupcakes, 12),
+        Pair(R.string.twenty_four_cupcakes, 24),
+
     )
 }
